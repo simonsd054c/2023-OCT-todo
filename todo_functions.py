de5184201682c9b2df5e3ac2c1f219866dc77359
Simon Dahal <simon.dahal@coderacademy.edu.au>
2023-12-09 10:57:36 +1100

    Create menu, add todo

---
+++ todo_functions.py
@@ -0,0 +1,21 @@
+import csv
+
+def add_todo(file_name):
+    print("Add todo")
+    # Ask the title of the todo
+    todo_name = input("Enter a todo: ")
+    # Open file - list.csv
+    with open(file_name, "a") as f:
+        writer = csv.writer(f)
+        # Insert values - title = user entered
+                      # - completed = False
+        writer.writerow([todo_name, "False"])
+
+def remove_todo(file_name):
+    print("Remove todo")
+
+def mark_todo(file_name):
+    print("Mark todo")
+
+def view_todo(file_name):
+    print("View todo")
